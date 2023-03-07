93ed5ddcb4719c17baf254b0d77f3c3e0330a3d8
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2023-03-07 16:50:50 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+// 1. Створення та рендер розмітки
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+  })
+  .join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", markup);
+
+// 2. modal
+
+galleryEl.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+});
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
